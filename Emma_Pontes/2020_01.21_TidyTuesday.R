18f3cfb56433af0ba444efc2688924fd7464f5e2
Emma Pontes <e.pontes1@umiami.edu>
2020-02-06 15:13:13 -0500

    tidy tuesday feb 6

---
+++ Emma_Pontes/2020_01.21_TidyTuesday.R
@@ -0,0 +1,16 @@
+# Get the Data
+
+spotify_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv')
+
+# Or read in with tidytuesdayR package (https://github.com/thebioengineer/tidytuesdayR)
+# PLEASE NOTE TO USE 2020 DATA YOU NEED TO UPDATE tidytuesdayR from GitHub
+
+# Either ISO-8601 date or year/week works!
+
+# Install via devtools::install_github("thebioengineer/tidytuesdayR")
+
+tuesdata <- tidytuesdayR::tt_load('2020-01-21') 
+tuesdata <- tidytuesdayR::tt_load(2020, week = 4)
+
+
+spotify_songs <- tuesdata$spotify_songs
