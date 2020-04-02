8d726976f7980fcc2d32f2f898ba7caac72990eb
mbrykow <mbrykow@users.noreply.github.com>
2020-04-02 14:58:15 -0400

    coding club 04/02

---
+++ Marissa_Rykowski/2020-03-31_Breweries.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/mbryk/Documents/RSMAS/tidytuesday/Marissa_Rykowski")
+
+brewing_materials <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/brewing_materials.csv')
+beer_taxed <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/beer_taxed.csv')
+brewer_size <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/brewer_size.csv')
+beer_states <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/beer_states.csv')
+
+library(tidyverse)
+
+list(unique(brewing_materials$material_type))
+
+###################################################################################################
+
+brewing_materials %>%
+  group_by(material_type)
